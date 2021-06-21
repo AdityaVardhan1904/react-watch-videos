9e45f5343c1753c4f70a59e3833d177358d3c513
Aditya Vardhan <cloverguy2001@gmail.com>
2021-06-21 23:38:52 +0530

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,34 +10,30 @@ const SearchBar = ({ onFormSubmit }) => {
   const onSubmit = (e) => {
     e.preventDefault();
     onFormSubmit(term);
-    this.setState({ term: "" });
+    setTerm("");
   };
+
+  return (
+    <div className="search-bar ui segment">
+      <form onSubmit={onSubmit} className="ui form">
+        <div style={{ display: "flex" }} className="ui field">
+          <label>Search Videos</label>
+          <input type="text" value={term} onChange={onInputChange} />
+          <button
+            style={{ marginLeft: "0.2rem" }}
+            className="ui secondary button"
+            onClick={this.onFormSubmit}
+          >
+            Search
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 class SearchBar extends React.Component {
-  render() {
-    return (
-      <div className="search-bar ui segment">
-        <form onSubmit={this.onFormSubmit} className="ui form">
-          <div style={{ display: "flex" }} className="ui field">
-            <label>Search Videos</label>
-            <input
-              type="text"
-              value={this.state.term}
-              onChange={this.onInputChange}
-            />
-            <button
-              style={{ marginLeft: "0.2rem" }}
-              className="ui secondary button"
-              onClick={this.onFormSubmit}
-            >
-              Search
-            </button>
-          </div>
-        </form>
-      </div>
-    );
-  }
+  render() {}
 }
 
 export default SearchBar;
